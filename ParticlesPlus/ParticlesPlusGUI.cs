53c1107e58c6012cea72142ff85ceb344e6e55ef
Staniboy <stanley.v@outlook.com>
2025-07-10 20:19:24 +0400

    feat: basic working GUI

--- ParticlesPlus/ParticlesPlusGUI.cs
+++ ParticlesPlus/ParticlesPlusGUI.cs
@@ -1,9 +1,13 @@
 ﻿using HarmonyLib;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.Client;
 using static System.Runtime.CompilerServices.RuntimeHelpers;
+using System;
 
 namespace ParticlesPlus
 {
@@ -11,7 +15,7 @@ namespace ParticlesPlus
     {
         public override string ToggleKeyCombinationCode => "particlesplus";
         private readonly ModConfig modConfig;
-
+  
         public MainGuiDialog(ICoreClientAPI capi, ModConfig config) : base(capi)
         {
             modConfig = config;
@@ -22,78 +26,123 @@ namespace ParticlesPlus
         }
         private void SetupDialog()
         {
-            int insetWidth = 900;
-            int insetHeight = 300;
-            int insetDepth = 3;
-            int rowHeight = 30;
-            int rowCount = 0;
+            int elementWidth = 300;
+            int labelHeight = 16;
+            int dropdownHeight = 30;
+            int spacing = 10;
+            int centeredLabelY = dropdownHeight / 2 - labelHeight / 2;
+
 
             // Auto-sized dialog at the center of the screen
             ElementBounds dialogBounds = ElementStdBounds.AutosizedMainDialog.WithAlignment(EnumDialogArea.CenterMiddle);
 
-            // Bounds of main inset for scrolling content in the GUI
-            ElementBounds insetBounds = ElementBounds.Fixed(0, GuiStyle.TitleBarHeight, insetWidth, insetHeight);
-            ElementBounds scrollbarBounds = insetBounds.RightCopy().WithFixedWidth(20);
+            // Preset Dropdown
+            ElementBounds presetDropdownLabelBounds = ElementBounds.Fixed(0, GuiStyle.TitleBarHeight, elementWidth, labelHeight);
+            ElementBounds presetDropdownBounds = ElementBounds.Fixed(0,0,elementWidth,dropdownHeight).FixedUnder(presetDropdownLabelBounds, spacing);
 
-            // Create child elements bounds for within the inset
-            ElementBounds clipBounds = insetBounds.ForkContainingChild(GuiStyle.HalfPadding, GuiStyle.HalfPadding, GuiStyle.HalfPadding, GuiStyle.HalfPadding);
-            ElementBounds containerBounds = insetBounds.ForkContainingChild(GuiStyle.HalfPadding, GuiStyle.HalfPadding, GuiStyle.HalfPadding, GuiStyle.HalfPadding);
-            ElementBounds containerRowBounds = ElementBounds.Fixed(0, 0, insetWidth, rowHeight).WithFixedMargin(10);
+            //// Particles Dropdown
+            ElementBounds particlesDropdownLabelBounds = ElementBounds.Fixed(0, 0, elementWidth, labelHeight).FixedUnder(presetDropdownBounds, spacing * 2);
+            ElementBounds particlesDropdownBounds = ElementBounds.Fixed(0,0,elementWidth,dropdownHeight).FixedUnder(particlesDropdownLabelBounds, spacing);
 
-            // Dialog background bounds
-            ElementBounds bgBounds = ElementBounds.Fill
-                .WithFixedPadding(GuiStyle.ElementToDialogPadding)
-                .WithSizing(ElementSizing.FitToChildren)
-                .WithChildren(insetBounds, scrollbarBounds);
+            //// Switch
+            ElementBounds switchBounds = ElementBounds.Fixed(0, 0, dropdownHeight, dropdownHeight).FixedUnder(particlesDropdownBounds, spacing * 2);
+            ElementBounds switchLabelBounds = ElementBounds.Fixed(0, centeredLabelY, elementWidth - dropdownHeight, labelHeight)
+                .FixedRightOf(switchBounds, spacing)
+                .FixedUnder(particlesDropdownBounds, spacing * 2);
 
-            // Create the dialog
-            SingleComposer = capi.Gui.CreateCompo("demoScrollGui", dialogBounds)
+            // Background boundaries. Again, just make it fit it's child elements, then add the text as a child element
+            ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
+            bgBounds.BothSizing = ElementSizing.FitToChildren;
+            bgBounds.WithChildren
+                (
+                presetDropdownLabelBounds, 
+                presetDropdownBounds, 
+                particlesDropdownLabelBounds, 
+                particlesDropdownBounds, 
+                switchBounds, 
+                switchLabelBounds
+                );
+
+            string[] presetValues = modConfig.Presets.Keys.ToArray();
+            string[] presetNames = modConfig.Presets.Keys.ToArray();
+            string[] particlesValues = modConfig.Particles.Keys.ToArray();
+            string[] particlesNames = modConfig.Particles.Keys.ToArray();
+
+            // Lastly, create the dialog
+            SingleComposer = capi.Gui.CreateCompo("myAwesomeDialog", dialogBounds)
                 .AddShadedDialogBG(bgBounds)
-                .AddDialogTitleBar("Particles Plus", OnTitleBarCloseClicked)
-                .BeginChildElements()
-                    .AddInset(insetBounds, insetDepth)
-                    .BeginClip(clipBounds)
-                        .AddContainer(containerBounds, "scroll-content")
-                    .EndClip()
-                    .AddVerticalScrollbar(OnNewScrollbarValue, scrollbarBounds, "scrollbar")
-                .EndChildElements();
+                .AddDialogTitleBar("Particles Plus Configuration", OnTitleBarCloseClicked)
+                .AddStaticText("Preset:",CairoFont.WhiteSmallText().WithLineHeightMultiplier(1.5f), presetDropdownLabelBounds)
+                .AddDropDown(presetValues, presetNames, 0, HandlePresetSelection, presetDropdownBounds, "presetDropdown")
+                .AddStaticText("Particles:", CairoFont.WhiteSmallText(), particlesDropdownLabelBounds)
+                .AddDropDown(particlesValues, particlesNames, 0, HandleParticlesSelection, particlesDropdownBounds, "particlesDropdown")
+                .AddSwitch(HandleEnabledSwitch, switchBounds, size: dropdownHeight, key: "enabledSwitch")
+                .AddStaticText("Enabled", CairoFont.WhiteSmallText(), switchLabelBounds)
+                .Compose();
 
-
-
-            // Add desired scrollable content to the container
-            GuiElementContainer scrollArea = SingleComposer.GetContainer("scroll-content");
-            foreach (var entry in modConfig.Presets)
-            {
-                scrollArea.Add(new GuiElementSwitch(capi, (bool boo) => { }, containerRowBounds));
-                scrollArea.Add(new GuiElementTextButton(capi, entry.Key, CairoFont.SmallButtonText(), CairoFont.WhiteSmallText(), () => { return true; }, containerRowBounds.RightCopy()));
-                containerRowBounds = containerRowBounds.BelowCopy();
-                rowCount++;
-                /*                scrollArea.Add(new GuiElementSwitch(capi, (bool boo) => { }, containerRowBounds.FlatCopy()));
-                                scrollArea.Add(new GuiElementTextButton(capi, entry.Key, CairoFont.SmallButtonText(), CairoFont.WhiteSmallText(), () => { return true; }, containerRowBounds.FlatCopy().WithFixedOffset(40,0)));
-                                containerRowBounds = containerRowBounds.BelowCopy();
-                                rowCount++;*/
-            }
-
-
-            // Compose the dialog
-            SingleComposer.Compose();
-
-            // After composing dialog, need to set the scrolling area heights to enable scroll behavior
-            float scrollVisibleHeight = (float)clipBounds.fixedHeight;
-            float scrollTotalHeight = rowHeight * rowCount;
-            SingleComposer.GetScrollbar("scrollbar").SetHeights(scrollVisibleHeight, scrollTotalHeight);
-
+            HandlePresetSelection(presetValues[0], true);
         }
-        private void OnNewScrollbarValue(float value)
+        private void HandlePresetSelection(string code, bool selected)
         {
-            ElementBounds bounds = SingleComposer.GetContainer("scroll-content").Bounds;
-            bounds.fixedY = 5 - value;
-            bounds.CalcWorldBounds();
-        }
+            string currentParticles = modConfig.Presets[code].Particles;
+            bool enabled = modConfig.Presets[code].Enabled;
 
+            SingleComposer.GetDropDown("particlesDropdown").SetSelectedValue(currentParticles);
+            SingleComposer.GetSwitch("enabledSwitch").SetValue(enabled);
+        }
+        private void HandleParticlesSelection(string code, bool selected)
+        {
+            string selectedPresetKey = SingleComposer.GetDropDown("presetDropdown").SelectedValue;
+            string wildCard = modConfig.Presets[selectedPresetKey].Mask;
+            modConfig.Presets[selectedPresetKey].Particles = code;
+
+            AdvancedParticleProperties[] particles = modConfig.Particles[code];
+            Block[] blocks = capi.World.SearchBlocks(wildCard);
+
+            RemoveParticles(blocks);
+            AddParticles(blocks, particles);
+        }
+        private void HandleEnabledSwitch(bool enabled)
+        {
+            string selectedPresetKey = SingleComposer.GetDropDown("presetDropdown").SelectedValue;
+            string particlesName = modConfig.Presets[selectedPresetKey].Particles;
+            string wildCard = modConfig.Presets[selectedPresetKey].Mask;
+            AdvancedParticleProperties[] particles = modConfig.Particles[particlesName];
+
+            modConfig.Presets[selectedPresetKey].Enabled = enabled;
+
+            Block[] blocks = capi.World.SearchBlocks(wildCard);
+
+            if (!enabled)
+            {
+                RemoveParticles(blocks);
+            }
+            else if (enabled) 
+            {
+                AddParticles(blocks, particles);
+            }
+                
+        }
         private void OnTitleBarCloseClicked()
         {
             TryClose();
+        }
+        private void RemoveParticles(Block[] blocks)
+        {
+            foreach (Block block in blocks)
+            {
+                block.ParticleProperties = Array.Empty<AdvancedParticleProperties>();
+            }
+        }
+        private void AddParticles(Block[] blocks, AdvancedParticleProperties[] particles)
+        {
+            foreach (Block block in blocks)
+            {
+                block.ParticleProperties ??= Array.Empty<AdvancedParticleProperties>();
+                block.ParticleProperties = block.ParticleProperties
+                                .Concat(particles)
+                                .ToArray();
+            }
         }
     } 
 }
