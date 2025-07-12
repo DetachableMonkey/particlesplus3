d8dff7a0365c4a10499dc34792816ba0d30ea67e
Staniboy <stanley.v@outlook.com>
2025-07-13 02:56:43 +0400

    feat: working GUI iteration

--- ParticlesPlus/ParticlesPlusGUI.cs
+++ ParticlesPlus/ParticlesPlusGUI.cs
@@ -8,20 +8,26 @@ using Vintagestory.API.Common;
 using Vintagestory.Client;
 using static System.Runtime.CompilerServices.RuntimeHelpers;
 using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ParticlesPlus
 {
     public class MainGuiDialog : GuiDialog
     {
         public override string ToggleKeyCombinationCode => "particlesplus";
+        private readonly ParticlesPlusModSystem modSystem;
         private readonly ModConfig modConfig;
-  
-        public MainGuiDialog(ICoreClientAPI capi, ModConfig config) : base(capi)
+        private PresetConfig selectedPreset;
+
+        public MainGuiDialog(ICoreClientAPI capi, ModConfig config, ParticlesPlusModSystem modSystem) : base(capi)
         {
-            modConfig = config;
+            this.modConfig = config;
+            this.modSystem = modSystem;
+            selectedPreset = null;
             SetupDialog();
             capi.Gui.RegisterDialog(this);
-            capi.Input.RegisterHotKey(ToggleKeyCombinationCode, "Particles Plus GUI", GlKeys.U, HotkeyType.GUIOrOtherControls);
+            capi.Input.RegisterHotKey(ToggleKeyCombinationCode, "Particles Plus GUI", GlKeys.P, HotkeyType.GUIOrOtherControls);
 
         }
         private void SetupDialog()
@@ -29,6 +35,7 @@ namespace ParticlesPlus
             int elementWidth = 300;
             int labelHeight = 16;
             int dropdownHeight = 30;
+            int buttonHeight = 40;
             int spacing = 10;
             int centeredLabelY = dropdownHeight / 2 - labelHeight / 2;
 
@@ -36,106 +43,222 @@ namespace ParticlesPlus
             // Auto-sized dialog at the center of the screen
             ElementBounds dialogBounds = ElementStdBounds.AutosizedMainDialog.WithAlignment(EnumDialogArea.CenterMiddle);
 
-            // Preset Dropdown
-            ElementBounds presetDropdownLabelBounds = ElementBounds.Fixed(0, GuiStyle.TitleBarHeight, elementWidth, labelHeight);
-            ElementBounds presetDropdownBounds = ElementBounds.Fixed(0,0,elementWidth,dropdownHeight).FixedUnder(presetDropdownLabelBounds, spacing);
+            // Key Input
+            ElementBounds keyInputLabelBounds = ElementBounds.Fixed(0, GuiStyle.TitleBarHeight, elementWidth, labelHeight);
+            ElementBounds keyInputBounds = ElementBounds.Fixed(0, 0, elementWidth, dropdownHeight).FixedUnder(keyInputLabelBounds, spacing);
 
-            //// Particles Dropdown
+            // Add New Button
+            ElementBounds addNewButtonBounds = ElementBounds.Fixed(0, 0, elementWidth, buttonHeight).FixedUnder(keyInputBounds, spacing);
+
+            // Preset Dropdown
+            ElementBounds presetDropdownLabelBounds = ElementBounds.Fixed(0, 0, elementWidth, labelHeight).FixedUnder(addNewButtonBounds, spacing * 2);
+            ElementBounds presetDropdownBounds = ElementBounds.Fixed(0, 0, elementWidth, dropdownHeight).FixedUnder(presetDropdownLabelBounds, spacing);
+
+            // Particles Dropdown
             ElementBounds particlesDropdownLabelBounds = ElementBounds.Fixed(0, 0, elementWidth, labelHeight).FixedUnder(presetDropdownBounds, spacing * 2);
-            ElementBounds particlesDropdownBounds = ElementBounds.Fixed(0,0,elementWidth,dropdownHeight).FixedUnder(particlesDropdownLabelBounds, spacing);
+            ElementBounds particlesDropdownBounds = ElementBounds.Fixed(0, 0, elementWidth, dropdownHeight).FixedUnder(particlesDropdownLabelBounds, spacing);
+
+            // Wildcard Input
+            ElementBounds wildcardInputLabelBounds = ElementBounds.Fixed(0, 0, elementWidth, labelHeight).FixedUnder(particlesDropdownBounds, spacing * 2);
+            ElementBounds wildcardInputBounds = ElementBounds.Fixed(0, 0, elementWidth, dropdownHeight).FixedUnder(wildcardInputLabelBounds, spacing);
 
             //// Switch
-            ElementBounds switchBounds = ElementBounds.Fixed(0, 0, dropdownHeight, dropdownHeight).FixedUnder(particlesDropdownBounds, spacing * 2);
-            ElementBounds switchLabelBounds = ElementBounds.Fixed(0, centeredLabelY, elementWidth - dropdownHeight, labelHeight)
+            ElementBounds switchBounds = ElementBounds.Fixed(0, 0, dropdownHeight, dropdownHeight).FixedUnder(wildcardInputBounds, spacing * 2);
+            ElementBounds switchLabelBounds = ElementBounds.Fixed(0, centeredLabelY, elementWidth - dropdownHeight - spacing, labelHeight)
                 .FixedRightOf(switchBounds, spacing)
-                .FixedUnder(particlesDropdownBounds, spacing * 2);
+                .FixedUnder(wildcardInputBounds, spacing * 2);
+
+            // Save Button
+            ElementBounds saveButtonBounds = ElementBounds.Fixed(0, 0, elementWidth / 2, buttonHeight).FixedUnder(switchBounds, spacing * 2);
+
+            // Delete Button
+            ElementBounds deleteButtonBounds = ElementBounds.Fixed(elementWidth / 2, 0, elementWidth / 2, buttonHeight).FixedUnder(switchBounds, spacing * 2);
 
             // Background boundaries. Again, just make it fit it's child elements, then add the text as a child element
             ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
             bgBounds.BothSizing = ElementSizing.FitToChildren;
             bgBounds.WithChildren
                 (
-                presetDropdownLabelBounds, 
-                presetDropdownBounds, 
-                particlesDropdownLabelBounds, 
-                particlesDropdownBounds, 
-                switchBounds, 
-                switchLabelBounds
+                presetDropdownLabelBounds,
+                presetDropdownBounds,
+                addNewButtonBounds,
+                keyInputLabelBounds,
+                keyInputBounds,
+                particlesDropdownLabelBounds,
+                particlesDropdownBounds,
+                wildcardInputLabelBounds,
+                wildcardInputBounds,
+                switchBounds,
+                switchLabelBounds,
+                saveButtonBounds,
+                deleteButtonBounds
                 );
-
-            string[] presetValues = modConfig.Presets.Keys.ToArray();
-            string[] presetNames = modConfig.Presets.Keys.ToArray();
-            string[] particlesValues = modConfig.Particles.Keys.ToArray();
-            string[] particlesNames = modConfig.Particles.Keys.ToArray();
 
             // Lastly, create the dialog
             SingleComposer = capi.Gui.CreateCompo("myAwesomeDialog", dialogBounds)
                 .AddShadedDialogBG(bgBounds)
                 .AddDialogTitleBar("Particles Plus Configuration", OnTitleBarCloseClicked)
-                .AddStaticText("Preset:",CairoFont.WhiteSmallText().WithLineHeightMultiplier(1.5f), presetDropdownLabelBounds)
-                .AddDropDown(presetValues, presetNames, 0, HandlePresetSelection, presetDropdownBounds, "presetDropdown")
+                .AddStaticText("Preset:", CairoFont.WhiteSmallText(), presetDropdownLabelBounds)
+                .AddDropDown(GetPresetNames(), GetPresetNames(), 0, OnPresetSelection, presetDropdownBounds, "presetDropdown")
+                .AddButton("Add New", OnAddNew, addNewButtonBounds)
+                .AddStaticText("Preset Name:", CairoFont.WhiteSmallText(), keyInputLabelBounds)
+                .AddTextInput(keyInputBounds, (string text) => { }, key: "keyInput")
                 .AddStaticText("Particles:", CairoFont.WhiteSmallText(), particlesDropdownLabelBounds)
-                .AddDropDown(particlesValues, particlesNames, 0, HandleParticlesSelection, particlesDropdownBounds, "particlesDropdown")
-                .AddSwitch(HandleEnabledSwitch, switchBounds, size: dropdownHeight, key: "enabledSwitch")
+                .AddDropDown(GetParticlesNames(), GetParticlesNames(), 0, null, particlesDropdownBounds, "particlesDropdown")
+                .AddStaticText("Wildcard:", CairoFont.WhiteSmallText(), wildcardInputLabelBounds)
+                .AddTextInput(wildcardInputBounds, (string text) => { }, key: "wildcardInput")
+                .AddSwitch(null, switchBounds, size: 30, key: "enabledSwitch")
                 .AddStaticText("Enabled", CairoFont.WhiteSmallText(), switchLabelBounds)
+                .AddButton("Save", OnSave, saveButtonBounds)
+                .AddButton("Delete", OnDelete, deleteButtonBounds)
+
                 .Compose();
 
-            HandlePresetSelection(presetValues[0], true);
+            OnPresetSelection(GetPresetNames()[0], true);
         }
-        private void HandlePresetSelection(string code, bool selected)
-        {
-            string currentParticles = modConfig.Presets[code].Particles;
-            bool enabled = modConfig.Presets[code].Enabled;
 
-            SingleComposer.GetDropDown("particlesDropdown").SetSelectedValue(currentParticles);
-            SingleComposer.GetSwitch("enabledSwitch").SetValue(enabled);
+        private string[] GetPresetNames()
+        {
+            string[] presetNames = modConfig.Presets.Keys.ToArray();
+
+            if (presetNames.Length == 0) return new string[] { "<none>" };
+
+            return presetNames; 
         }
-        private void HandleParticlesSelection(string code, bool selected)
+        private string[] GetParticlesNames()
         {
-            string selectedPresetKey = SingleComposer.GetDropDown("presetDropdown").SelectedValue;
-            string wildCard = modConfig.Presets[selectedPresetKey].Mask;
-            modConfig.Presets[selectedPresetKey].Particles = code;
-
-            AdvancedParticleProperties[] particles = modConfig.Particles[code];
-            Block[] blocks = capi.World.SearchBlocks(wildCard);
-
-            RemoveParticles(blocks);
-            AddParticles(blocks, particles);
+            return modConfig.Particles.Keys.ToArray();
         }
-        private void HandleEnabledSwitch(bool enabled)
+        private void OnPresetSelection(string code, bool selected)
         {
-            string selectedPresetKey = SingleComposer.GetDropDown("presetDropdown").SelectedValue;
-            string particlesName = modConfig.Presets[selectedPresetKey].Particles;
-            string wildCard = modConfig.Presets[selectedPresetKey].Mask;
-            AdvancedParticleProperties[] particles = modConfig.Particles[particlesName];
-
-            modConfig.Presets[selectedPresetKey].Enabled = enabled;
-
-            Block[] blocks = capi.World.SearchBlocks(wildCard);
-
-            if (!enabled)
+            if (code == "<none>")
             {
-                RemoveParticles(blocks);
-            }
-            else if (enabled) 
+                selectedPreset = null;
+                SetFormClear();
+            } 
+            else
             {
-                AddParticles(blocks, particles);
+                selectedPreset = modConfig.Presets[code];
+                SingleComposer.GetDropDown("presetDropdown").SetSelectedValue(code);
+                SingleComposer.GetDropDown("particlesDropdown").SetSelectedValue(selectedPreset.Particles);
+                SingleComposer.GetSwitch("enabledSwitch").SetValue(selectedPreset.Enabled);
+                SingleComposer.GetTextInput("wildcardInput").SetValue(selectedPreset.Wildcard);
             }
-                
+        }
+        private void SetFormClear()
+        {
+            SingleComposer.GetDropDown("presetDropdown").SetSelectedIndex(0);
+            SingleComposer.GetDropDown("particlesDropdown").SetSelectedIndex(0);
+            SingleComposer.GetSwitch("enabledSwitch").SetValue(false);
+            SingleComposer.GetTextInput("wildcardInput").SetValue("");
         }
         private void OnTitleBarCloseClicked()
         {
             TryClose();
         }
-        private void RemoveParticles(Block[] blocks)
+        private bool OnAddNew()
         {
+            GuiElementTextInput keyInput = SingleComposer.GetTextInput("keyInput");
+            string newKeyName = keyInput.GetText();
+            if (!string.IsNullOrWhiteSpace(newKeyName))
+            {
+                if (!modConfig.Presets.ContainsKey(newKeyName))
+                {
+                    modConfig.Presets[newKeyName] = new PresetConfig
+                    {
+                        Enabled = false,
+                        Wildcard = "",
+                        Particles = null
+                    };
+                    GuiElementDropDown presetDropdown = SingleComposer.GetDropDown("presetDropdown");
+                    string[] presetNames = GetPresetNames();
+                    presetDropdown.SetList(presetNames, presetNames);
+                    OnPresetSelection(newKeyName, true);
+                }
+            }
+            keyInput.SetValue("");
+
+            return true;
+        }
+        private bool OnSave()
+        {
+            if (selectedPreset == null) return false;
+
+            string name = SingleComposer.GetTextInput("keyInput").Text;
+            string particles = SingleComposer.GetDropDown("particlesDropdown").SelectedValue;
+            string wildcard = SingleComposer.GetTextInput("wildcardInput").GetText();
+            bool enabled = SingleComposer.GetSwitch("enabledSwitch").On;
+
+            if (selectedPreset.Enabled && enabled)
+            {
+                if (selectedPreset.Wildcard != wildcard || selectedPreset.Particles != particles)
+                {
+                    RemoveParticles(selectedPreset.Wildcard);
+                    AddParticles(wildcard, modConfig.Particles[particles]);
+                }
+            }
+
+            if (selectedPreset.Enabled && !enabled)
+            {
+                RemoveParticles(wildcard);
+            }
+
+            if (!selectedPreset.Enabled && enabled)
+            {
+                AddParticles(wildcard, modConfig.Particles[particles]);
+            }
+
+            selectedPreset.Particles = particles;
+            selectedPreset.Wildcard = wildcard;
+            selectedPreset.Enabled = enabled;
+            modSystem.WriteConfig(capi);
+            return true;
+            
+        }
+        private bool OnDelete() //TODO: Make this work.
+        {
+            GuiElementDropDown presetDropdown = SingleComposer.GetDropDown("presetDropdown");
+            string keyToDelete = presetDropdown.SelectedValue;
+
+            if (keyToDelete == "<none>")
+            {
+                return false;
+            }
+
+            // Remove key particles
+            RemoveParticles(modConfig.Presets[keyToDelete].Wildcard);
+            // Remove Key
+            modConfig.Presets.Remove(keyToDelete);
+            // Update Presets List for Dropdown
+            string[] presetNames = GetPresetNames();
+            // Set updated list to dropdown
+            UpdatePresetDropdownList(presetDropdown);
+            // Set form to first element in updated list if empty reset form.
+            OnPresetSelection(presetNames[0], true);
+
+            modSystem.WriteConfig(capi);
+            return true;
+        }
+        private void UpdatePresetDropdownList(GuiElementDropDown presetDropdown)
+        {
+            string[] presetNames = GetPresetNames();
+            presetDropdown.SetList(presetNames, presetNames);
+        }
+        private Block[] GetBlocks(string wildcard)
+        {
+            return capi.World.SearchBlocks(wildcard);
+        }
+        private void RemoveParticles(string wildcard)
+        {
+            Block[] blocks = GetBlocks(wildcard);
             foreach (Block block in blocks)
             {
                 block.ParticleProperties = Array.Empty<AdvancedParticleProperties>();
             }
         }
-        private void AddParticles(Block[] blocks, AdvancedParticleProperties[] particles)
+        private void AddParticles(string wildcard, AdvancedParticleProperties[] particles)
         {
+            Block[] blocks = GetBlocks(wildcard);
             foreach (Block block in blocks)
             {
                 block.ParticleProperties ??= Array.Empty<AdvancedParticleProperties>();
