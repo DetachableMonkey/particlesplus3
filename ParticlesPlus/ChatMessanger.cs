b815f6044307bd8fc65295a14f561ebbfcc222e8
Staniboy <stanley.v@outlook.com>
2025-07-31 12:19:25 +0400

    feat: add ChatMessanger Class

---
+++ ParticlesPlus/ChatMessanger.cs
@@ -0,0 +1,39 @@
+﻿using Vintagestory.API.Client;
+
+namespace ParticlesPlus
+{
+    enum MessageType
+    {
+        Fail,
+        Success        
+    }
+    internal class ChatMessanger
+    {
+        private readonly ModSystem _modSystem;
+        private readonly ICoreClientAPI _capi;
+
+        private readonly string _modName;
+        private readonly string successColor = "#5FED6F";
+        private readonly string failColor = "#ED5E60";
+
+
+        public ChatMessanger(ICoreClientAPI capi, ModSystem modSystem)
+        {
+            _capi = capi;
+            _modSystem = modSystem;
+            _modName = _modSystem.Mod.Info.Name;
+        }
+
+        public void ShowMessage(string message, MessageType type)
+        {
+            string messageColor = type switch
+            {
+                MessageType.Success => successColor,
+                MessageType.Fail => failColor,
+                _ => "#FFFFFF",
+            };
+
+            _capi.ShowChatMessage($"<font color='{messageColor}'>[{_modName}]{message}</font>");
+        }
+    }
+}
